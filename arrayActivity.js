d74bfa887d91ff2bfb0fca55ae5994fc4e60f946
John Bryan Lomuntad <pudgeuser259@gmail.com>
2024-10-16 07:06:25 +0800

    Add files via upload

---
+++ arrayActivity.js
@@ -0,0 +1,12 @@
+const favSingers = ["Eminem", "Arnel Pineda", "Bruno Mars"];
+
+console.log(favSingers[0]);
+
+const favNumbers = [5, 17, 4, 20];
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
